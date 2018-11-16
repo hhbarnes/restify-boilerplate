5bcc995603ed7118bd3842caaa6168af71556f21
Harry Barnes <hhbarnes@users.noreply.github.com>
2018-11-16 16:33:15 -0500

    added password

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const timestamps = require('mongoose-timestamp');
+const bcrypt = require('mongoose-bcrypt');
+
+require('mongoose-type-email');
 
 const UserSchema = new mongoose.Schema({
 	email: {
-		type: String,
+		type: mongoose.SchemaTypes.Email,
 		trim: true,
 		lowercase: true,
 		unique: true,
@@ -21,8 +24,14 @@ const UserSchema = new mongoose.Schema({
 			required: true,
 		},
 	},
+	password: {
+		type: String,
+		bcrypt: true,
+		required: true,
+	},
 }, { collection: 'users' });
 
 UserSchema.plugin(timestamps);
+UserSchema.plugin(bcrypt, { rounds: 10 });
 
 module.exports = mongoose.model('User', UserSchema);
